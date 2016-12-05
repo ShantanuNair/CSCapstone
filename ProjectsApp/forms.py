f9f28b71a047b9314041ae8a7bf487b75739f4e5
jaideep <juneja0@purdue.edu>
2016-12-05 02:37:55 -0500

    Basic Project Skeleton added
    BAsic ProjectsApp structure

---
+++ ProjectsApp/forms.py
@@ -0,0 +1,10 @@
+"""GroupsApp Forms
+
+Created by Naman Patwari on 10/10/2016.
+"""
+from django import forms
+
+class ProjectForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=30)
+    description = forms.CharField(label='Description', max_length=300)
+
