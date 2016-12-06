b5e14a9d412d23a11348916c8b34503262b7f025
jaideep <juneja0@purdue.edu>
2016-12-05 20:59:17 -0500

    Project form now takes all inputs and sets them to db except company

--- ProjectsApp/forms.py
+++ ProjectsApp/forms.py
@@ -7,4 +7,6 @@ from django import forms
 class ProjectForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
     description = forms.CharField(label='Description', max_length=300)
-    skills = forms.CharField(label='Enter Skills (in CSV)', max_length=300)
+    skills = forms.CharField(label='skills', max_length=300)
+    experience = forms.CharField(label='experience', max_length=300)
+    specialty = forms.CharField(label='specialty', max_length=50)
